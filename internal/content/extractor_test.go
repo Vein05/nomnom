c68570a535a6e3cd85daa1235c571e93687e7569
Sugam Panthi <man359905@gmail.com>
2025-03-18 14:49:35 -0500

    Enhance file processing and size conversion in the nomnom package
    - Updated ProcessDirectory function to accept a configuration parameter for improved file handling.
    - Introduced convertSize function to handle size conversions from string representations (e.g., "100MB", "100KB").
    - Added unit tests for convertSize to ensure correct functionality across various size formats.
    - Improved logging for file processing to provide clearer context and error handling.

--- internal/content/extractor_test.go
+++ internal/content/extractor_test.go
@@ -1,18 +1,17 @@
 package nomnom
 
 import (
-	"fmt"
+	utils "nomnom/internal/utils"
 	"testing"
 )
 
 func TestProcessDirectory(t *testing.T) {
 
 	path := "/Users/vein/Documents/nomnom/internal/content/demo"
-	query, err := ProcessDirectory(path)
+	query, err := ProcessDirectory(path, utils.Config{})
 	if err != nil {
 		t.Fatalf("ProcessDirectory failed: %v", err)
 	}
-	fmt.Printf("Query: %+v\n", query)
 
 	// check if there are two files in the query
 	if len(query.Folders[0].FileList) != 2 {
@@ -26,6 +25,49 @@ func TestProcessDirectory(t *testing.T) {
 
 	if query.Folders[0].FileList[1].Name != "def.txt" {
 		t.Fatalf("Expected def.txt, got %s", query.Folders[0].FileList[1].Name)
+	}
+
+}
+
+func TestConvertSize(t *testing.T) {
+	// test 100MB
+	size := "100MB"
+	convertedSize, err := convertSize(size)
+	if err != nil {
+		t.Fatalf("convertSize failed: %v", err)
+	}
+	if convertedSize != 100*MB {
+		t.Fatalf("Expected 100MB, got %d", convertedSize)
+	}
+
+	// test 100KB
+	size = "100KB"
+	convertedSize, err = convertSize(size)
+	if err != nil {
+		t.Fatalf("convertSize failed: %v", err)
+	}
+	if convertedSize != 100*KB {
+		t.Fatalf("Expected 100KB, got %d", convertedSize)
+	}
+
+	// test 100GB
+	size = "100GB"
+	convertedSize, err = convertSize(size)
+	if err != nil {
+		t.Fatalf("convertSize failed: %v", err)
+	}
+	if convertedSize != 100*GB {
+		t.Fatalf("Expected 100GB, got %d", convertedSize)
+	}
+
+	// test 100B
+	size = "100B"
+	convertedSize, err = convertSize(size)
+	if err != nil {
+		t.Fatalf("convertSize failed: %v", err)
+	}
+	if convertedSize != 100 {
+		t.Fatalf("Expected 100B, got %d", convertedSize)
 	}
 
 }
