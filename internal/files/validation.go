92902165ea82974377e01230ee5da1fc4242ef61
Sugam Panthi <man359905@gmail.com>
2025-03-18 14:51:11 -0500

    Add RefinedName function to sanitize file names
    - Introduced RefinedName function to remove new lines, spaces, and specific unwanted characters (e.g., "```", "```plaintext") from file names.
    - Improved file name handling to ensure cleaner and more consistent naming conventions.

--- internal/files/validation.go
+++ internal/files/validation.go
@@ -1,1 +1,20 @@
 package nomnom
+
+import "strings"
+
+func RefinedName(name string) string {
+	// remove new lines and spaces from the name
+	name = strings.ReplaceAll(name, "\n", "")
+	name = strings.ReplaceAll(name, " ", "")
+
+	// remove any ``` from the name
+	name = strings.ReplaceAll(name, "```", "")
+
+	//remove any ```plaintext from the name
+	name = strings.ReplaceAll(name, "```plaintext", "")
+
+	//remove any ``` from the name
+	name = strings.ReplaceAll(name, "```", "")
+
+	return name
+}
